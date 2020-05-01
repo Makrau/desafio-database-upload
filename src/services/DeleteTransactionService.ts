a3560930c6918626295f8999b8d43cadd66ba729
Iago Goncalves <iago.goncalves@klickpages.com.br>
2020-05-01 15:31:39 -0300

    deletando uma transacao

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(transactionId: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const transaction = await transactionsRepository.findOne(transactionId);
+
+    if (!transaction) {
+      throw new AppError('Transaction not found');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
